5d4da67db9fb97dd5ca804f760d7e4b8d4c3dffc
Joel Cooper <jodycooper16@gmail.com>
2021-09-01 11:27:49 -0700

    commit tree 47ee9a19730be98957317112ac2b25db47a72021

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,14 +7,25 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    session[:pageviews_remaining] ||=3
+    session[:pageviews_remaining] -= 1
+    
+    
+    if
+      session[:pageviews_remaining] > 0
     article = Article.find(params[:id])
     render json: article
+    else 
+      render json: {error: "Maximum pageview limit reached"}, status: 
+    :unauthorized
+  end
   end
 
   private
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
+
   end
 
 end
